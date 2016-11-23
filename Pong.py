06fc4e85bb30baffb92da6128801343b88a7a3d3
abustos09 <abustos09@gmail.com>
2016-11-22 16:39:17 -0800

    updated file

--- Pong.py
+++ Pong.py
@@ -1,285 +1,154 @@
- # program template for Spaceship
-import simplegui
-import math
+# Implementation of classic arcade game Pong
+
+import simpleguitk as simplegui
 import random
 
-# globals for user interface
-WIDTH = 800
-HEIGHT = 600
-score = 0
-lives = 3
-time = 0
+# initialize globals - pos and vel encode vertical info for paddles
+WIDTH = 600
+HEIGHT = 400       
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH / 2
+HALF_PAD_HEIGHT = PAD_HEIGHT / 2
+paddle1_pos = HEIGHT / 2
+paddle2_pos = HEIGHT / 2
+paddle1_vel = 0
+paddle2_vel = 0
+LEFT = False
+RIGHT = True
+ball_pos = [WIDTH / 2, HEIGHT / 2]
+scoreleft = 0
+scoreright = 0
+paddle_acc = 2
 
-class ImageInfo:
-    def __init__(self, center, size, radius = 0, lifespan = None, animated = False):
-        self.center = center
-        self.size = size
-        self.radius = radius
-        if lifespan:
-            self.lifespan = lifespan
-        else:
-            self.lifespan = float('inf')
-        self.animated = animated
-
-    def get_center(self):
-        return self.center
-
-    def get_size(self):
-        return self.size
-
-    def get_radius(self):
-        return self.radius
-
-    def get_lifespan(self):
-        return self.lifespan
-
-    def get_animated(self):
-        return self.animated
-
+# initialize ball_pos and ball_vel for new bal in middle of table
+# if direction is RIGHT, the ball's velocity is upper right, else upper left
+def spawn_ball(direction):
+    # Random velocity generated. 
+    global ball_pos, ball_vel
+    ball_pos = [WIDTH / 2, HEIGHT / 2]
+    random_horiz_vel_x = (random.randrange(120, 240) / 60.0)
+    random_vert_vel_y = (random.randrange(60, 80) / 60.0)
     
-# art assets created by Kim Lathrop, may be freely re-used in non-commercial projects, please credit Kim
+    if direction == True:
+        ball_vel = [random_horiz_vel_x, - random_vert_vel_y]
+    elif direction == False:
+        ball_vel = [- random_horiz_vel_x, - random_vert_vel_y]
     
-# debris images - debris1_brown.png, debris2_brown.png, debris3_brown.png, debris4_brown.png
-#                 debris1_blue.png, debris2_blue.png, debris3_blue.png, debris4_blue.png, debris_blend.png
-debris_info = ImageInfo([320, 240], [640, 480])
-debris_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/debris2_blue.png")
-
-# nebula images - nebula_brown.png, nebula_blue.png
-nebula_info = ImageInfo([400, 300], [800, 600])
-nebula_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_blue.f2014.png")
-
-# splash image
-splash_info = ImageInfo([200, 150], [400, 300])
-splash_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/splash.png")
-
-# ship image
-ship_info = ImageInfo([45, 45], [90, 90], 35)
-ship_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/double_ship.png")
-
-# missile image - shot1.png, shot2.png, shot3.png
-missile_info = ImageInfo([5,5], [10, 10], 3, 50)
-missile_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/shot2.png")
-
-# asteroid images - asteroid_blue.png, asteroid_brown.png, asteroid_blend.png
-asteroid_info = ImageInfo([45, 45], [90, 90], 40)
-asteroid_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_blue.png")
-
-# animated explosion - explosion_orange.png, explosion_blue.png, explosion_blue2.png, explosion_alpha.png
-explosion_info = ImageInfo([64, 64], [128, 128], 17, 24, True)
-explosion_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/explosion_alpha.png")
-
-# sound assets purchased from sounddogs.com, please do not redistribute
-soundtrack = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/soundtrack.mp3")
-missile_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/missile.mp3")
-missile_sound.set_volume(.5)
-ship_thrust_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/thrust.mp3")
-explosion_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/explosion.mp3")
-
-# alternative upbeat soundtrack by composer and former IIPP student Emiel Stopler
-# please do not redistribute without permission from Emiel at http://www.filmcomposer.nl
-#soundtrack = simplegui.load_sound("https://storage.googleapis.com/codeskulptor-assets/ricerocks_theme.mp3")
-
-# helper functions to handle transformations
-def angle_to_vector(ang):
-    return [math.cos(ang), math.sin(ang)]
-
-def dist(p,q):
-    return math.sqrt((p[0] - q[0]) ** 2+(p[1] - q[1]) ** 2)
-
-
-# Ship class
-class Ship:
-    def __init__(self, pos, vel, angle, image, info):
-        self.pos = [pos[0],pos[1]]
-        self.vel = [vel[0],vel[1]]
-        self.thrust = False
-        self.angle = angle
-        self.angle_vel = 0
-        self.image = image
-        self.image_center = info.get_center()
-        self.image_size = info.get_size()
-        self.radius = info.get_radius()
-        
-    def draw(self,canvas):
-        #canvas.draw_circle(self.pos, self.radius, 1, "White", "White")
-        canvas.draw_image(self.image, self.image_center, self.image_size, \
-            self.pos, self.image_size, self.angle)
-        
-
-    def update(self):
-        # angular roatation 
-        self.angle += self.angle_vel
-        velocity_vector = angle_to_vector(self.angle)
-
-        # class velocity update by means of vector velocity in vector direction
-        if self.thrust == True:
-            self.vel[0] += velocity_vector[0] * .048
-            self.vel[1] += velocity_vector[1] * .048
-        else:
-            self.vel[0] *= .98
-            self.vel[1] *= .98
-
-        # thruster flames on/off 
-        if self.thrust == True:
-            self.image_center = [135, 45]
-            self.pos[0] = (self.pos[0] + self.vel[0]) % WIDTH
-            self.pos[1] = (self.pos[1] + self.vel[1]) % HEIGHT
-        else:
-            self.image_center = [45, 45] 
-            self.pos[0] = (self.pos[0] + self.vel[0]) % WIDTH
-            self.pos[1] = (self.pos[1] + self.vel[1]) % HEIGHT
-        
-        # thruster sound
-        if self.thrust == True:
-            ship_thrust_sound.play()
-        else:
-            ship_thrust_sound.rewind()
-
-    def missiles(self):
-        global a_missile
-        a_missile = Sprite([self.pos[0] , self.pos[1]], self.vel, self.angle , 0, missile_image, missile_info, missile_sound)
-        """
-        draws but doesnt update the position
-        """
-
-
-# Sprite class
-class Sprite:
-    def __init__(self, pos, vel, ang, ang_vel, image, info, sound = None):
-        self.pos = [pos[0],pos[1]]
-        self.vel = [vel[0],vel[1]]
-        self.angle = ang
-        self.angle_vel = ang_vel
-        self.image = image
-        self.image_center = info.get_center()
-        self.image_size = info.get_size()
-        self.radius = info.get_radius()
-        self.lifespan = info.get_lifespan()
-        self.animated = info.get_animated()
-        self.age = 0
-        if sound:
-            sound.rewind()
-            sound.play()
-        
-        """
-        if self.angle_vel == 0:
-            velocity_vector = angle_to_vector(self.angle)
-            self.vel[0] += velocity_vector[0] * 5
-            self.vel[1] += velocity_vector[1] * 5
-        """
-
-    def draw(self, canvas):
-        #canvas.draw_circle(self.pos, self.radius, 1, "Red", "Red")
-        canvas.draw_image(self.image, self.image_center, self.image_size, self.pos, self.image_size, self.angle)
- 
-    def update(self):
-        # spin. 0 spin for missile (through angle_vel = 0). angle will dictate angle missile 
-        # will shoot. angle_vel > 0 will add spin to meteor
-
-        self.angle += self.angle_vel  
-        
-        if self.angle_vel == 0:
-            velocity_vector = angle_to_vector(self.angle)
-            self.vel[0] += velocity_vector[0] * 1.1
-            self.vel[1] += velocity_vector[1] * 1.1
-
-
-        self.pos[0] = (self.pos[0] + self.vel[0]) % 800
-        self.pos[1] = (self.pos[1] + self.vel[1]) % 600
-  
+# define event handlers
+def new_game():
+    global paddle_acc, paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, scoreleft, scoreright 
+    scoreleft = 0
+    scoreright = 0
+    paddle_acc = 2
+    paddle1_vel = 0
+    paddle2_vel = 0
+    paddle1_pos = HEIGHT / 2
+    paddle2_pos = HEIGHT / 2
+    direction = random.choice([LEFT, RIGHT])
+    spawn_ball(direction)
 
 def draw(canvas):
-    global time
-    
-    # animiate background
-    time += 1
-    wtime = (time / 4) % WIDTH
-    center = debris_info.get_center()
-    size = debris_info.get_size()
-    canvas.draw_image(nebula_image, nebula_info.get_center(), nebula_info.get_size(), [WIDTH / 2, HEIGHT / 2], [WIDTH, HEIGHT])
-    canvas.draw_image(debris_image, center, size, (wtime - WIDTH / 2, HEIGHT / 2), (WIDTH, HEIGHT))
-    canvas.draw_image(debris_image, center, size, (wtime + WIDTH / 2, HEIGHT / 2), (WIDTH, HEIGHT))
-
-    # draw ship and sprites
-    my_ship.draw(canvas)
-    a_rock.draw(canvas)
-    a_missile.draw(canvas)
-    
-    # update ship and sprites
-    my_ship.update()
-    a_rock.update()
-    a_missile.update()
-
-    #
-    canvas.draw_text("Score: " + str(score), [WIDTH - 80, HEIGHT - 25], 20, "white")
-    canvas.draw_text("Lives: " + str(lives), [WIDTH - 80, HEIGHT - 5], 20, "white")
+    global paddle_acc, scoreleft, scoreright, paddle1_pos, paddle2_pos, ball_pos, ball_vel
+ 
+    # draw mid line and gutters
+    canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
+    canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
+    canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
+        
+    # update ball
+    ball_pos[0] += ball_vel[0]
+    ball_pos[1] += ball_vel[1]
             
-# timer handler that spawns a rock    
-def rock_spawner():
-    global a_rock, asteroid_image
-    # rock position 
-    rock_width = random.randrange(1, WIDTH)
-    rock_height = random.randrange(1, HEIGHT)
-    #rock velocity 
-    lower = -1
-    rock_vel = [random.random() * 2 + lower, random.random() * 2 + lower]
-    # rock angle vel
-    rock_angle_vel = random.random() * .2 + -.1
-    # all 3 asteroids
-    asteroid_image_A = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_blend.png")
-    asteroid_image_B = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_blue.png")
-    asteroid_image_C = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_brown.png")
-    asteroid_image = random.choice([asteroid_image_A,asteroid_image_B, asteroid_image_C])
-    # Sprite class call
-    a_rock = Sprite([rock_width, rock_height], rock_vel, 1, rock_angle_vel , asteroid_image, asteroid_info)
-
-# Button handler
-def keydown(key):
-
-    if key == simplegui.KEY_MAP['up']:
-        #my_ship.vel[0] = 1
-        my_ship.thrust = True
-        
-
-    elif key == simplegui.KEY_MAP['left']:
-        my_ship.angle_vel = -.05
-        #my_ship.vel[1] = -1
-
-    elif key == simplegui.KEY_MAP['right']:
-        my_ship.angle_vel = .05
-        #my_ship.vel[1] = 1
-
-    if key == simplegui.KEY_MAP['space']:
-        my_ship.missiles()
-
-def keyup(key):
-
-    if key == simplegui.KEY_MAP['up']:
-        my_ship.thrust = False
-        
-    elif key == simplegui.KEY_MAP['left']:
-        my_ship.angle_vel = 0
-    elif key == simplegui.KEY_MAP['right']:
-        my_ship.angle_vel = 0
-
+    # draw ball
+    canvas.draw_circle(ball_pos, BALL_RADIUS, 1, "white", "white")
     
-# initialize frame
-frame = simplegui.create_frame("Asteroids", WIDTH, HEIGHT)
+    # update paddle's vertical position, keep paddle on the screen
+    if paddle1_pos <= HALF_PAD_HEIGHT:
+        paddle1_pos = HALF_PAD_HEIGHT
+    elif paddle1_pos >= HEIGHT - HALF_PAD_HEIGHT:
+        paddle1_pos = HEIGHT - HALF_PAD_HEIGHT
+    
+    if paddle2_pos <= HALF_PAD_HEIGHT:
+        paddle2_pos = HALF_PAD_HEIGHT
+    elif paddle2_pos >= HEIGHT - HALF_PAD_HEIGHT:
+        paddle2_pos = HEIGHT - HALF_PAD_HEIGHT
+        
+    paddle1_pos += paddle1_vel
+    paddle2_pos += paddle2_vel
+                
+    # draw paddles
+    canvas.draw_line([1, paddle1_pos - HALF_PAD_HEIGHT],\
+                     [1, paddle1_pos + HALF_PAD_HEIGHT], PAD_WIDTH, "white")
+    canvas.draw_line([WIDTH - 1, paddle2_pos - HALF_PAD_HEIGHT ],\
+                     [WIDTH - 1, paddle2_pos + HALF_PAD_HEIGHT], PAD_WIDTH, "white")
+    
+    # determine whether paddle and ball collide 
+    if (ball_pos[0] - BALL_RADIUS <= HALF_PAD_WIDTH) and paddle1_pos - HALF_PAD_HEIGHT <= ball_pos[1] <= paddle1_pos + HALF_PAD_HEIGHT:
+        ball_vel[0] =  - ball_vel[0] * 1.1
+        paddle_acc = paddle_acc * 1.1
+    elif (ball_pos[0] + BALL_RADIUS) >= WIDTH - HALF_PAD_WIDTH and paddle2_pos - HALF_PAD_HEIGHT <= ball_pos[1] <= paddle2_pos + HALF_PAD_HEIGHT:
+        ball_vel[0] =  - ball_vel[0] * 1.1
+        paddle_acc = paddle_acc * 1.1
+    
+    # determine whether balls collide with gutter
+    if (ball_pos[0] <= PAD_WIDTH):
+        scoreright += 1
+        paddle_acc = 2
+        spawn_ball(RIGHT)
+    elif (ball_pos[0] >= WIDTH - PAD_WIDTH):
+        scoreleft += 1
+        paddle_acc = 2
+        spawn_ball(LEFT)
 
-# initialize ship and two sprites
-my_ship = Ship([WIDTH / 2, HEIGHT / 2], [0, 0], 0, ship_image, ship_info)
-a_rock = Sprite([WIDTH / 3, HEIGHT / 3], [1, 1], 0, .07, asteroid_image, asteroid_info)
-#a_missile = Sprite([2 * WIDTH / 3, 2 * HEIGHT / 3], [-1,1], 0, 0, missile_image, missile_info, missile_sound)
-a_missile = Sprite([2 * WIDTH / 3, 2 * HEIGHT / 3], [0, 0], 0, 0, missile_image, missile_info, missile_sound)
+    # determine whether balls collide with top and bottom walls
+    if ball_pos[1] <= BALL_RADIUS:
+        ball_vel[1] = - ball_vel[1]
+    elif ball_pos[1] >= HEIGHT - BALL_RADIUS:
+        ball_vel[1] = - ball_vel[1]
+    
+    # draw scores
+    canvas.draw_text(str(scoreleft), [200, 100], 48, "white")
+    canvas.draw_text(str(scoreright), [400, 100], 48, "white")
+        
+def keydown(key):
+    global paddle1_vel, paddle2_vel
 
-# register handlers
+    if key == simplegui.KEY_MAP["W"]:
+        paddle1_vel -= paddle_acc 
+    elif key == simplegui.KEY_MAP["S"]:
+        paddle1_vel += paddle_acc 
+    
+    if key == simplegui.KEY_MAP["up"]:
+        paddle2_vel -= paddle_acc
+    elif key == simplegui.KEY_MAP["down"]:
+        paddle2_vel += paddle_acc 
+    
+   
+def keyup(key):
+    global paddle1_vel, paddle2_vel
+    if key == simplegui.KEY_MAP["W"]:
+        paddle1_vel = 0
+    elif key == simplegui.KEY_MAP["S"]:
+        paddle1_vel = 0
+    if key == simplegui.KEY_MAP["up"]:
+        paddle2_vel = 0
+    elif key == simplegui.KEY_MAP["down"]:
+        paddle2_vel = 0
+        
+def restart():
+    # Button handler that calls new_game()
+    new_game()
+    
+
+# create frame
+frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 frame.set_keyup_handler(keyup)
+frame.add_button("New Game", restart, 200)
 
-
-timer = simplegui.create_timer(1500.0, rock_spawner)
-
-# get things rolling
-timer.start()
+# start frame
+new_game()
 frame.start()
